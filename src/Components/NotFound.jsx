335ee02ebb2279302feae093635af5e44496e56a
Anicet-AGBOSSOU <anicetagbossou5@gmail.com>
2025-04-11 19:57:22 +0100

    ADD

---
+++ src/Components/NotFound.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function NotFound() {
+  return (
+    <div> 
+      <h1> 404 NotFound</h1>
+    <span>Cette page n'existe pas sur notre site, Revenez à la page d'Accueil s'il vous plait</span>
+
+    </div>
+  )
+}
+
+export default NotFound
